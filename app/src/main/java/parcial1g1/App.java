08bb272b3636e949438d55b75cc0756174cbf1ee
OsorioP7 <thomas.osorio.passos@gmail.com>
2024-08-22 07:14:57 -0500

    Commit parcial
    Thomas Osorio Passos

--- app/src/main/java/parcial1g1/App.java
+++ app/src/main/java/parcial1g1/App.java
@@ -4,8 +4,56 @@ package parcial1g1;
 public class App {
 
     public static void main(String[] args) {
+    
+        try {
         
-        // Implementar la logica de solucion
+        Scanner sc = new Scanner(System.in);
+
+            int cantidadVehiculos = 0;
+            int distanciaMetros = 0;
+            float kmRecorrido = 0f;
+            double consumoCombustible = 0;
+            double consumoTotalGasolina = 0;
+
+        System.out.println("Cuantos vehiculos partiparon:  ");
+        cantidadVehiculos = sc.nextInt();
+        sc.nextLine();
+
+        if (cantidadVehiculos > 0) {
+            
+            for(int i = 0; i < cantidadVehiculos; i++) {
+
+                System.out.println("ingrese la distancia recorrida por el vehiculo " + (i + 1) + " en metros ");
+                distanciaMetros = sc.nextInt();
+                sc.nextLine();
+                
+                kmRecorrido = (float)(distanciaMetros / 1000);
+                consumoCombustible = calcularConsumoGasolina(kmRecorrido);
+
+                System.out.println("El vehiculo " + (i + 1) + " recorrio " + kmRecorrido + " km | consumio " + consumoCombustible + " Litros de gasolina ");
+
+                consumoTotalGasolina += consumoCombustible;
+        
+            }
+
+        System.out.println("El consumo total de combustible de todos los vehículos es de " + consumoTotalGasolina + " litros.");
+
+        }
+        
+        else {
+
+            System.out.println("la cantidad de vehiculos debe ser mayor a 0");
+
+        }
+       
+        } 
+        
+        catch (Exception e) {
+            
+            System.out.println("Error: " + e.getMessage());
+            
+        }
+
         
     }
 
@@ -17,8 +65,29 @@ public class App {
     */
     //------------------------------------------------------------------
     //Implemente la funcion en este espacio
-    
+    public static float calcularConsumoGasolina(float kmRecorrido){
+       
+        try {
 
+        float consumoGasolina = 0f;
+
+        if (kmRecorrido > 0) {
+            
+            consumoGasolina = ((kmRecorrido * 55) / 750);
+
+        }
+        else{
+
+            System.out.println("no hay kilometraje");
+
+        }
+        
+        return consumoGasolina;
+
+       } catch (Exception e) {
+        
+        return -1;
+       } 
+    }
     //------------------------------------------------------------------
-
 }
